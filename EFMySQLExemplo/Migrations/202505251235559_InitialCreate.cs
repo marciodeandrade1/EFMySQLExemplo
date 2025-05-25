8a32795355e3c4c966ba7cd0f8435f4bd87091ba
gmand <gmand@DESKTOP-2KO8PEB>
2025-05-25 08:41:10 -0400

    Ajustes no App.config e Enable-Migrations,  Add-Migrations InitialCreate e Update-Database para criação do banco de dados e tabela clientes no MySql

---
+++ EFMySQLExemplo/Migrations/202505251235559_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace EFMySQLExemplo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 100, storeType: "nvarchar"),
+                        Email = c.String(nullable: false, maxLength: 100, storeType: "nvarchar"),
+                        DataCadastro = c.DateTime(nullable: false, precision: 0),
+                        Ativo = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clientes");
+        }
+    }
+}
---
+++ EFMySQLExemplo/Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+//using MySql.Data.Entity; // Add this using
+using MySql.Data.EntityFramework;
+
+namespace EFMySQLExemplo.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<EFMySQLExemplo.Contexto.MeuContexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySqlMigrationSqlGenerator());
+        }
+
+        protected override void Seed(EFMySQLExemplo.Contexto.MeuContexto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
